bff0f533ccc0b878f989f1ff02dbe1ca306488c9
rocketyen <rocketyen@gmail.com>
2021-11-10 21:38:15 +0100

    commit

--- views/users/profilePatient.php
+++ views/users/profilePatient.php
@@ -1,22 +1,21 @@
-
 <main>
     <?= $errMess ?? '' ?>
     <div class="container">
-        <?php if(!empty ($errMess)){
-            echo $errMess
-            ;}else{ ?>
-        <h1 class="text-center text-white mt-3 mb-2">patient</h1>
-        <table class="offset-2 col-8  mt-3 mb-2">
-            <thead class="text-light bg-dark mt-3 mb-2 text-center">
-                <tr>
-                    <th>Nom</th>
-                    <th>Prénom</th>
-                    <th>Date de naissance</th>
-                    <th>N° de Téléphone</th>
-                    <th>Adresse mail</th>
-                </tr>
-            </thead>
-            <tbody class="text-light bg-dark mt-3 mb-2 text-center">                
+        <?php if (!empty($errMess)) {
+            echo $errMess;
+        } else { ?>
+            <h1 class="text-center text-white mt-3 mb-2">patient</h1>
+            <table class="offset-2 col-8  mt-3 mb-2">
+                <thead class="text-light bg-dark mt-3 mb-2 text-center">
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Date de naissance</th>
+                        <th>N° de Téléphone</th>
+                        <th>Adresse mail</th>
+                    </tr>
+                </thead>
+                <tbody class="text-light bg-dark mt-3 mb-2 text-center">
                     <tr>
                         <td><?= $result->firstname; ?></td>
                         <td><?= $result->lastname; ?></td>
@@ -24,10 +23,10 @@
                         <td><?= $result->phone; ?></td>
                         <td><?= $result->mail; ?></td>
                         <td><a href="/controllers/updatePatientController.php?patient=<?= $result->id ?>">
-                        <button>Modifier les informations du patient</button></a></td>
+                                <button>Modifier les informations du patient</button></a></td>
                     </tr>
-            </tbody>
-        </table>
-    <?php }?>
+                </tbody>
+            </table>
+        <?php } ?>
     </div>
 </main>
